9b80653ec4e90ef5ab8940796c21aca3739d920e
Bengt Ericsson <zolomon@gmail.com>
2014-04-26 13:22:50 +0200

    initial commit

---
+++ Assets/Scripts/Agressor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agressor : MonoBehaviour {
+	public Vector3 Direction {get;set;}
+	public float Speed {get;set;}
+	private int xDir {get;set;}
+	private int yDir {get;set;}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Protector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class Protector : MonoBehaviour {
+	public Vector3 Direction { get; set; }
+	public float Speed { get; set; }
+	private int xDir { get; set; }
+	private int yDir { get;set; }
+
+	// Use this for initialization
+	void Start () {
+		Speed = 1f;
+		Direction = new Vector3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			yDir = 1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.S)) {
+			yDir = -1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			xDir = -1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.D)) {
+			xDir = 1;
+		}
+
+		Direction = new Vector3 (xDir, yDir, 0);
+
+		Direction.Normalize ();
+
+		transform.localPosition = currentPosition + Direction * Speed * Time.deltaTime;
+	}
+}
