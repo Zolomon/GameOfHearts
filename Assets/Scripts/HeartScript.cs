17bc3ab8ba6f85ead68243d577e54eda2303ab28
Cousken <cousken@gmail.com>
2014-04-27 14:13:40 +0200

    Bunch of changes

--- Assets/Scripts/HeartScript.cs
+++ Assets/Scripts/HeartScript.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class HeartScript : MonoBehaviour {
 
+    public Animator animator = null;
+    public Animation myAnimation = null;
 	// Use this for initialization
 	void Start () {
-		gameObject.animation.playAutomatically = false;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
     internal void ColidedWithBacteria()
     {
-     
+        animator.Play("heart2");
     }
 }
